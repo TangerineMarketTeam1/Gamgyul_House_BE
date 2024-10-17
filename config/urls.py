48388cb6a1f96674f0af976a3703478a00ac6bf1
Seong-jae.Park <bvc3435@gamil.com>
2024-10-17 18:05:04 +0900

    ✨ feat : 인증방식 변경
    세션인증 -> JWT 토큰 인증으로 변경

--- config/urls.py
+++ config/urls.py
@@ -11,6 +11,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("accounts/", include("allauth.urls")),
     path("chats/", include("chats.urls")),
+    path("market/", include("market.urls")),
     path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
