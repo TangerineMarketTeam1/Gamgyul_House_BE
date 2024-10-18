877836bae9f296df9a80a5e9e4b2eff32eb5f4f4
lwj0712 <dldnjswot@naver.com>
2024-10-18 10:53:00 +0900

    🔧 conf: chats앱 변경사항
    - admin에서 chats 관리 탭 추가
    - 이미지 저장 경로 수정(S3), 마이그레이션 필요
    - import문 전체적으로 일관성있게 변경
    - url 패턴 수정

--- chats/serializers.py
+++ chats/serializers.py
@@ -1,6 +1,6 @@
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
-from .models import ChatRoom, Message
+from chats.models import *
 
 User = get_user_model()
 
