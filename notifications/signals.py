99ef077f76e6b9615f8390a2cb6555c24af8ab79
lwj0712 <dldnjswot@naver.com>
2024-10-23 21:28:32 +0900

    ♻️ refactor: signals.py 리팩토링
    - 알람 중복 생성 문제를 방지할 수 있게끔 케이스를 분리

--- notifications/signals.py
+++ notifications/signals.py
@@ -24,75 +24,89 @@ def create_notification_for_new_message(sender, instance, created, **kwargs):
                 .first()
             )
 
-            # WebSocket 연결 기록이 없거나, 마지막 연결이 종료된 후 메시지가 온 경우에만 알림 생성
-            if not last_connection or (
-                last_connection.disconnected_at
-                and last_connection.disconnected_at < instance.sent_at
-            ):
-                notification = Notification.objects.create(
+            if not last_connection or last_connection.disconnected_at is None:
+                # WebSocket이 연결되어 있지 않으면 알림 생성
+                continue
+            elif last_connection.disconnected_at < instance.sent_at:
+                # WebSocket 연결 종료 후 메시지가 온 경우에만 알림 생성
+                if not Notification.objects.filter(
                     recipient=recipient,
                     sender=instance.sender,
                     notification_type="message",
-                    message=f"{instance.sender.username}님이 새로운 메시지를 보냈습니다.",
                     related_object_id=instance.id,
-                )
-                send_notification_via_websocket(
-                    Notification, notification, created=True
-                )
+                ).exists():
+                    notification = Notification.objects.create(
+                        recipient=recipient,
+                        sender=instance.sender,
+                        notification_type="message",
+                        message=f"{instance.sender.username}님이 새로운 메시지를 보냈습니다.",
+                        related_object_id=instance.id,
+                    )
+                    send_notification_via_websocket(
+                        Notification, notification, created=True
+                    )
 
 
 @receiver(post_save, sender=Comment)
 def create_notification_for_new_comment(sender, instance, created, **kwargs):
-    """
-    새로운 댓글이 달렸을 때 알림을 생성하는 신호
-    """
     if created:
         recipient = instance.post.user
         sender = instance.user
 
-        Notification.objects.create(
+        if not Notification.objects.filter(
             recipient=recipient,
             sender=sender,
             notification_type="comment",
-            message=f"{sender.username}님이 게시물에 댓글을 남겼습니다.",
             related_object_id=instance.post.id,
-        )
+        ).exists():
+            Notification.objects.create(
+                recipient=recipient,
+                sender=sender,
+                notification_type="comment",
+                message=f"{sender.username}님이 게시물에 댓글을 남겼습니다.",
+                related_object_id=instance.post.id,
+            )
 
 
 @receiver(post_save, sender=Follow)
 def create_notification_for_new_follower(sender, instance, created, **kwargs):
-    """
-    새로운 팔로우가 발생할 때 알림을 생성하는 신호
-    """
     if created:
         recipient = instance.following
         sender = instance.follower
 
-        Notification.objects.create(
+        if not Notification.objects.filter(
             recipient=recipient,
             sender=sender,
             notification_type="follow",
-            message=f"{sender.username}님이 당신을 팔로우했습니다.",
-            related_object_id=None,  # 팔로우는 관련 객체가 없으므로 None
-        )
+        ).exists():
+            Notification.objects.create(
+                recipient=recipient,
+                sender=sender,
+                notification_type="follow",
+                message=f"{sender.username}님이 당신을 팔로우했습니다.",
+                related_object_id=None,
+            )
 
 
 @receiver(post_save, sender=Like)
 def create_notification_for_new_like(sender, instance, created, **kwargs):
-    """
-    좋아요가 눌렸을 때 알림을 생성하는 신호
-    """
     if created:
         recipient = instance.post.user
         sender = instance.user
 
-        Notification.objects.create(
+        if not Notification.objects.filter(
             recipient=recipient,
             sender=sender,
             notification_type="like",
-            message=f"{sender.username}님이 게시물을 좋아합니다.",
             related_object_id=instance.post.id,
-        )
+        ).exists():
+            Notification.objects.create(
+                recipient=recipient,
+                sender=sender,
+                notification_type="like",
+                message=f"{sender.username}님이 게시물을 좋아합니다.",
+                related_object_id=instance.post.id,
+            )
 
 
 @receiver(post_save, sender=Notification)
