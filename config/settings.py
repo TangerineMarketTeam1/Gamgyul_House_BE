132fe171ff78c241ce84b97347a66d4156d52b1b
Donggyu-Kim <kimdg000411@gmail.com>
2024-11-07 14:50:46 +0900

    :wrench: conf: allowed host 변경, debug=True로 변경

--- config/settings.py
+++ config/settings.py
@@ -10,9 +10,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv("SECRET_KEY")
 
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = [
+    "3.35.220.139",
+    "localhost",
+    "127.0.0.1",
+]
 
 INSTALLED_APPS = [
     # 3rd party apps
